77d4505a7d05dbff6903f719d82d05f65bbf4903
Yuyang Zhang(helloqiu) <helloqiu95@gmail.com>
2017-12-26 19:30:09 +0800

    生成加入七号线之后的图

--- code/convert_weight_to_label.py
+++ code/convert_weight_to_label.py
@@ -3,9 +3,14 @@
 import csv
 import os
 
-if __name__ == "__main__":
+
+def convert_graph(with_7th=True):
+    if not with_7th:
+        file_name = "gephi_edge"
+    else:
+        file_name = "gephi_edge_with_7th"
     l = list()
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/gephi_edge.csv'), 'r',
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/{}.csv'.format(file_name)), 'r',
               encoding='utf-8') as f:
         r = csv.DictReader(f)
         names = r.fieldnames
@@ -14,8 +19,13 @@ if __name__ == "__main__":
     for item in l:
         item['Label'] = item['Weight']
 
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/gephi_edge_with_label.csv'), 'a',
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/{}_with_label.csv'.format(file_name)),
+              'a',
               encoding='utf-8') as f:
         w = csv.DictWriter(f, fieldnames=names)
         w.writeheader()
         w.writerows(l)
+
+
+if __name__ == "__main__":
+    convert_graph(with_7th=True)
