b022fffe28dc1b0f2a581b2cbf1557a6d30aaa8b
Yuyang Zhang <helloqiu95@gmail.com>
2018-01-01 21:29:44 +0800

    加入了新的图片

--- code/generate_photo.py
+++ code/generate_photo.py
@@ -1,15 +1,18 @@
 from generate_relative_position import relative_position
 import svgwrite
+from graph import subway_graph
+
+pos = relative_position()
 
 
 def draw_map():
-    pos = relative_position()
-    dwg = svgwrite.Drawing('../picture/地铁位置图.svg', profile='full', size=('4000', '4000'))
+    dwg = svgwrite.Drawing('../picture/地铁位置图.svg', profile='full', size=('2000', '3000'))
     for i in pos:
         dwg.add(svgwrite.shapes.Circle(
             center=(i['latitude'], i['longitude']),
             fill='#8fddff',
-            r=5
+            r=8,
+            fill_opacity="0.5"
         ))
         dwg.add(svgwrite.text.Text(
             text=i['name'],
@@ -18,7 +21,30 @@ def draw_map():
             font_family="黑体",
             style="text-anchor: middle; dominant-baseline: central;"
         ))
+    g = subway_graph()
+    for e in g.edges.data():
+        pos_a = get_location(e[0])
+        pos_b = get_location(e[1])
+        dwg.add(svgwrite.path.Path(
+            stroke_width="1",
+            stroke_opacity="0.5",
+            stroke="#ffaa5c",
+            d="M {},{} L {},{}".format(pos_a[0], pos_a[1], pos_b[0], pos_b[1])
+        ))
+        dwg.add(svgwrite.text.Text(
+            text=e[2]['weight'],
+            font_size="5",
+            font_family="黑体",
+            style="text-anchor: middle; dominant-baseline: central;",
+            insert=((pos_a[0] + pos_b[0]) / 2, (pos_a[1] + pos_b[1]) / 2)
+        ))
     dwg.save()
+
+
+def get_location(name):
+    for i in pos:
+        if i['name'] == name:
+            return i['latitude'], i['longitude']
 
 
 if __name__ == '__main__':
