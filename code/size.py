a14ed6b6365ab7cea521a30b810ce6e6ac925f05
Yuyang Zhang <helloqiu95@gmail.com>
2018-01-05 06:13:54 +0800

    加入了规模

---
+++ code/size.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from graph import graph_by_date
+import os
+import csv
+
+base_dir = os.path.join(os.path.dirname(__file__), '../data/')
+
+
+def size():
+    result = graph_by_date()
+    l = list()
+    for k in result.keys():
+        g = result[k]
+        l.append({
+            'date': k.strftime("%Y-%m-%d"),
+            'size': len(g.nodes)
+        })
+    with open(os.path.join(base_dir, '分阶段数据/规模.csv'), 'a') as f:
+        w = csv.DictWriter(f, ['date', 'size'])
+        w.writeheader()
+        w.writerows(l)
+
+
+if __name__ == "__main__":
+    size()
