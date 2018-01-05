57a407459a4d0d7f9dcbf45367d04a638d0f1893
Yuyang Zhang(helloqiu) <helloqiu95@gmail.com>
2018-01-05 22:17:33 +0800

    加入变化量

--- code/chart/size_diameter.py
+++ code/chart/size_diameter.py
@@ -35,28 +35,51 @@ def bc_data():
     return result
 
 
-def delta_bc():
-    bc = bc_data()
+def get_delta(data):
     delta = list()
-    last = bc[0]
-    for i in range(1, len(bc)):
-        delta.append(bc[i] - last)
-        last = bc[i]
+    last = data[0]
+    for i in range(1, len(data)):
+        delta.append(data[i] - last)
+        last = data[i]
     return delta
 
 
-def draw_size_diameter():
+def shortest_path_data():
+    path = list()
+    with open(os.path.join(base_dir, "分阶段数据/平均最短路径.csv"), 'r', encoding='utf-8') as f:
+        r = csv.DictReader(f)
+        for row in r:
+            path.append(float(row['average_shortest_path_length']))
+    return path
+
+
+def draw_size_diameter_shortest_path():
     sns.set(style='ticks', palette='Set2')
     diameter = diameter_data()
     size = size_data()
     plt.figure(dpi=200)
     x1, y1 = zip(*sorted(zip(size, diameter)))
-    plt.plot(x1, y1)
+    plt.plot(x1, y1, label='diameter')
     plt.plot(x1, y1, 'o')
-    plt.ylabel('L')
+    shortest_path = shortest_path_data()
+    x2, y2 = zip(*sorted(zip(size, shortest_path)))
+    plt.plot(x2, y2, label='average length')
+    plt.plot(x2, y2, 'o')
+
+    size.remove(size[0])
+    size.remove(size[len(size) - 1])
+    delta_diameter = get_delta(diameter)
+    x3, y3 = zip(*sorted(zip(size, delta_diameter)))
+    plt.plot(x3, y3, '^', label='delta diameter')
+    delta_shortest_path = get_delta(shortest_path)
+    x4, y4 = zip(*sorted(zip(size, delta_shortest_path)))
+    plt.plot(x4, y4, '^', label='delta average length')
+
     plt.xlabel('N')
+    plt.legend(loc='best')
 
     ax = plt.gca()
+    ax.spines['bottom'].set_position(('data', 0))
     ax.spines['top'].set_color('none')
     ax.spines['right'].set_color('none')
 
@@ -71,7 +94,7 @@ def draw_size_bc():
     x1, y1 = zip(*sorted(zip(size, bc)))
     plt.plot(x1, y1, label='BC')
     plt.plot(x1, y1, 'o')
-    delta = delta_bc()
+    delta = get_delta(bc)
     size.remove(size[0])
     size.remove(size[len(size) - 1])
     x2, y2 = zip(*sorted(zip(size, delta)))
@@ -88,4 +111,4 @@ def draw_size_bc():
 
 
 if __name__ == "__main__":
-    draw_size_bc()
+    draw_size_diameter_shortest_path()
