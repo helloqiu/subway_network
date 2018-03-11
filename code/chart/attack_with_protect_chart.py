34dc26be36eb9796a536d19352b5f7c5acba99eb
Yuyang Zhang <helloqiu95@gmail.com>
2018-03-12 07:43:36 +0800

    set MultipleLocator

--- code/chart/attack_with_protect_chart.py
+++ code/chart/attack_with_protect_chart.py
@@ -5,6 +5,7 @@ import csv
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from matplotlib.ticker import MultipleLocator
 
 base_dir = os.path.join(os.path.dirname(__file__), '../../data/')
 
@@ -42,6 +43,9 @@ def largest_degree_attack_with_highest_bt_protect_chart():
     ax = plt.gca()
     ax.spines['top'].set_color('none')
     ax.spines['right'].set_color('none')
+    xmajorLocator = MultipleLocator(0.1)
+    ax.xaxis.set_major_locator(xmajorLocator)
+    ax.yaxis.set_major_locator(xmajorLocator)
     plt.xlabel('Fraction of the removed nodes')
     plt.ylabel('Fraction of the protected nodes')
     plt.show()
