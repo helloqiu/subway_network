8fba197126aeaaf9fbf567e895601d3bebad51b6
Yuyang Zhang <helloqiu95@gmail.com>
2018-03-12 06:22:42 +0800

    add simplified attack_with_protect_efficiency

--- code/chart/attack_with_protect.py
+++ code/chart/attack_with_protect.py
@@ -6,21 +6,24 @@ from attack import random_attack_list, largest_degree_attack_list, highest_bc_at
 from graph import subway_graph
 from networkx.algorithms.efficiency import global_efficiency
 
-d = 0.01
+d = 0.02
 base_dir = os.path.join(os.path.dirname(__file__), '../../data/')
 
 
-def highest_bt_attack_with_highest_bc_attack_efficiency():
+def attack_with_protect_efficiency(attack, protect):
     result = list()
-    for i in range(0, 50):
+    attack_name = attack.__name__.replace('_attack_list', '')
+    protect_name = protect.__name__.replace('_attack_list', '')
+    for i in range(0, 25):
+        print('{} attack with {} protect:'.format(attack_name, protect_name))
         attack_fraction = d * i
         print('attack fraction: %.2f' % attack_fraction)
-        for j in range(0, 50):
+        for j in range(0, 25):
             g = subway_graph()
-            attack_list = highest_bc_attack_list(g, fraction=attack_fraction)
+            attack_list = attack(g, fraction=attack_fraction)
             protect_fraction = d * j
             print('\t protect fraction: %.2f' % protect_fraction)
-            protect_list = highest_bt_attack_list(g, fraction=protect_fraction)
+            protect_list = protect(g, fraction=protect_fraction)
             for node in attack_list:
                 if node not in protect_list:
                     g.remove_node(node)
@@ -29,11 +32,14 @@ def highest_bt_attack_with_highest_bc_attack_efficiency():
                 'protect_fraction': "%.2f" % protect_fraction,
                 'efficiency': global_efficiency(g)
             })
-    with open(os.path.join(base_dir, 'attack/highest_bt_attack_with_highest_bc_attack_efficiency.csv'), 'a') as f:
+    with open(os.path.join(base_dir, 'attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
+              'a') as f:
         w = csv.DictWriter(f, ['attack_fraction', 'protect_fraction', 'efficiency'])
         w.writeheader()
         w.writerows(result)
 
 
 if __name__ == "__main__":
-    highest_bt_attack_with_highest_bc_attack_efficiency()
+    attack_with_protect_efficiency(attack=largest_degree_attack_list, protect=highest_bt_attack_list)
+    attack_with_protect_efficiency(attack=highest_bc_attack_list, protect=highest_bt_attack_list)
+    attack_with_protect_efficiency(attack=highest_bt_attack_list, protect=highest_bt_attack_list)
