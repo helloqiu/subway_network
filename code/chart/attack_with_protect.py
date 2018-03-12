a9872fa003fd4d34472444c6020d06b9a2a68ea8
Yuyang Zhang(helloqiu) <helloqiu95@gmail.com>
2018-03-12 22:28:59 +0800

    add more results

--- code/chart/attack_with_protect.py
+++ code/chart/attack_with_protect.py
@@ -11,6 +11,37 @@ base_dir = os.path.join(os.path.dirname(__file__), '../../data/')
 
 
 def attack_with_protect_efficiency(attack, protect):
+    attack_name = attack.__name__.replace('_attack_list', '')
+    protect_name = protect.__name__.replace('_attack_list', '')
+    result = get_result(attack, protect)
+    with open(os.path.join(base_dir, 'attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
+              'a') as f:
+        w = csv.DictWriter(f, ['attack_fraction', 'protect_fraction', 'efficiency'])
+        w.writeheader()
+        w.writerows(result)
+
+
+def random_related_efficiency(attack, protect):
+    attack_name = attack.__name__.replace('_attack_list', '')
+    protect_name = protect.__name__.replace('_attack_list', '')
+    result = None
+    for i in range(0, 10):
+        if result is None:
+            result = get_result(attack, protect)
+        else:
+            temp = get_result(attack, protect)
+            for r in range(0, len(result)):
+                result[r]['efficiency'] += temp[r]['efficiency']
+    for i in result:
+        i['efficiency'] = i['efficiency'] / 10
+    with open(os.path.join(base_dir, 'attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
+              'a') as f:
+        w = csv.DictWriter(f, ['attack_fraction', 'protect_fraction', 'efficiency'])
+        w.writeheader()
+        w.writerows(result)
+
+
+def get_result(attack, protect):
     result = list()
     attack_name = attack.__name__.replace('_attack_list', '')
     protect_name = protect.__name__.replace('_attack_list', '')
@@ -32,14 +63,8 @@ def attack_with_protect_efficiency(attack, protect):
                 'protect_fraction': "%.2f" % protect_fraction,
                 'efficiency': global_efficiency(g)
             })
-    with open(os.path.join(base_dir, 'attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
-              'a') as f:
-        w = csv.DictWriter(f, ['attack_fraction', 'protect_fraction', 'efficiency'])
-        w.writeheader()
-        w.writerows(result)
+    return result
 
 
 if __name__ == "__main__":
-    attack_with_protect_efficiency(attack=largest_degree_attack_list, protect=largest_degree_attack_list)
-    attack_with_protect_efficiency(attack=highest_bc_attack_list, protect=largest_degree_attack_list)
-    attack_with_protect_efficiency(attack=highest_bt_attack_list, protect=largest_degree_attack_list)
+    random_related_efficiency(attack=random_attack_list, protect=random_attack_list)
