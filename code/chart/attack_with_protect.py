8db0cd6a83b90f69c3df6d6e0c0c7552635540c4
Yuyang Zhang(helloqiu) <helloqiu95@gmail.com>
2018-03-26 16:43:29 +0800

    加入更多成都attack&protect的数据和图表

--- code/chart/attack_with_protect.py
+++ code/chart/attack_with_protect.py
@@ -16,7 +16,7 @@ def attack_with_protect_efficiency(attack, protect):
     attack_name = attack.__name__.replace('_attack_list', '')
     protect_name = protect.__name__.replace('_attack_list', '')
     result = get_result(attack, protect)
-    with open(os.path.join(base_dir, 'shanghai_attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
+    with open(os.path.join(base_dir, 'attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
               'a') as f:
         w = csv.DictWriter(f, ['attack_fraction', 'protect_fraction', 'efficiency'])
         w.writeheader()
@@ -36,7 +36,7 @@ def random_related_efficiency(attack, protect):
                 result[r]['efficiency'] += temp[r]['efficiency']
     for i in result:
         i['efficiency'] = i['efficiency'] / 10
-    with open(os.path.join(base_dir, 'shanghai_attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
+    with open(os.path.join(base_dir, 'attack/{}_attack_with_{}_protect.csv').format(attack_name, protect_name),
               'a') as f:
         w = csv.DictWriter(f, ['attack_fraction', 'protect_fraction', 'efficiency'])
         w.writeheader()
@@ -52,8 +52,8 @@ def get_result(attack, protect):
         attack_fraction = d * i
         print('attack fraction: %.2f' % attack_fraction)
         for j in range(0, 25):
-            # g = get_chengdu_subway_graph()
-            g = get_shanghai_subway_graph()
+            g = get_chengdu_subway_graph()
+            # g = get_shanghai_subway_graph()
             attack_list = attack(g, fraction=attack_fraction)
             protect_fraction = d * j
             print('\t protect fraction: %.2f' % protect_fraction)
@@ -71,20 +71,20 @@ def get_result(attack, protect):
 
 if __name__ == "__main__":
     t_list = list()
-    t = Process(target=attack_with_protect_efficiency, args=(largest_degree_attack_list, highest_bt_attack_list))
-    t_list.append(t)
-
-    t = Process(target=attack_with_protect_efficiency,
-                args=(shanghai_highest_bc_attack_list, highest_bt_attack_list))
-    t_list.append(t)
-
-    t = Process(target=attack_with_protect_efficiency,
-                args=(highest_bt_attack_list, highest_bt_attack_list))
+    t = Process(target=random_related_efficiency, args=(largest_degree_attack_list, random_attack_list))
     t_list.append(t)
 
     t = Process(target=random_related_efficiency,
-                args=(random_attack_list, highest_bt_attack_list))
+                args=(highest_bc_attack_list, random_attack_list))
     t_list.append(t)
+
+    t = Process(target=random_related_efficiency,
+                args=(highest_bt_attack_list, random_attack_list))
+    t_list.append(t)
+
+    # t = Process(target=random_related_efficiency,
+    #            args=(random_attack_list, highest_bt_attack_list))
+    # t_list.append(t)
 
     for t in t_list:
         t.start()
